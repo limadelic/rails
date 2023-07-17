0ca5720ea84c51ab71706774238fb2b715d4a5f0
msuarz <mikemps@gmail.com>
2023-07-17 15:06:32 -0400

    cleanup

--- cs/tests/Examples/Login.cs
+++ cs/tests/Examples/Login.cs
@@ -5,11 +5,11 @@ using Password = Messages.User.Pass;
 
 public abstract class Login
 {
-    string? User { get; set; }
-    string? Pass { get; set; }
+    protected string? User { get; set; }
+    protected string? Pass { get; set; }
 
-    readonly int MaxLength = 6;
-    readonly int MinLength = 3;
+    protected readonly int MaxLength = 6;
+    protected readonly int MinLength = 3;
 
     protected abstract Result SetCreds(string user, string pass);
         
@@ -41,41 +41,40 @@ public abstract class Login
         SetCreds(user, pass)
             .Should().BeError(error);
     }
+}
 
-    [TestFixture]
-    public class ResultOnly : Login
-    {
-        protected override Result SetCreds(string user, string pass) => 
+[TestFixture]
+public class ResultOnly : Login
+{
+    protected override Result SetCreds(string user, string pass) => 
             
-            Return.Ok
-                .If(() => !string.IsNullOrWhiteSpace(user), Name.Required)
-                .Do(() => user = user.Trim())
-                .If(() => user.Length >= MinLength, Name.MinLength)
-                .If(() => user.Length <= MaxLength, Name.MaxLength)
-                .If(() => pass is not null, Password.Required)
-                .If(() => pass.Length >= MinLength, Password.MinLength)
-                .If(() => pass.Length <= MaxLength, Password.MaxLength)
-                .Do(() => User = user)
-                .Do(() => Pass = pass);
-    }
+        Return.Ok
+            .If(() => !string.IsNullOrWhiteSpace(user), Name.Required)
+            .Do(() => user = user.Trim())
+            .If(() => user.Length >= MinLength, Name.MinLength)
+            .If(() => user.Length <= MaxLength, Name.MaxLength)
+            .If(() => pass is not null, Password.Required)
+            .If(() => pass.Length >= MinLength, Password.MinLength)
+            .If(() => pass.Length <= MaxLength, Password.MaxLength)
+            .Do(() => User = user)
+            .Do(() => Pass = pass);
+}
 
-    [TestFixture]
-    public class WithValues : Login
-    {
-        protected override Result SetCreds(string user, string pass) => 
+[TestFixture]
+public class WithValues : Login
+{
+    protected override Result SetCreds(string user, string pass) => 
             
-            Return.Ok
-                .With(user)
-                .If(user => !string.IsNullOrWhiteSpace(user), Name.Required)
-                .Do(user => user.Trim())
-                .If(user => user.Length >= MinLength, Name.MinLength)
-                .If(user => user.Length <= MaxLength, Name.MaxLength)
-                .Do(user => User = user)
-                .With(pass)
-                .If(pass => pass is not null, Password.Required)
-                .If(pass => pass.Length >= MinLength, Password.MinLength)
-                .If(pass => pass.Length <= MaxLength, Password.MaxLength)
-                .Do(pass => Pass = pass);
-    }
-    
+        Return.Ok
+            .With(user)
+            .If(user => !string.IsNullOrWhiteSpace(user), Name.Required)
+            .Do(user => user.Trim())
+            .If(user => user.Length >= MinLength, Name.MinLength)
+            .If(user => user.Length <= MaxLength, Name.MaxLength)
+            .Do(user => User = user)
+            .With(pass)
+            .If(pass => pass is not null, Password.Required)
+            .If(pass => pass.Length >= MinLength, Password.MinLength)
+            .If(pass => pass.Length <= MaxLength, Password.MaxLength)
+            .Do(pass => Pass = pass);
 }
